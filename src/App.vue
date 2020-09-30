1792e6b694391765cb379cf636dbd49963e73774
Dusty <wajtejprotl@outlook.com>
2020-09-30 20:54:47 +0800

    创建 Icon

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
   padding : 0;
   box-sizing : border-box;
 }
+a {
+  text-decoration : none;
+  color : #333333;
+}
 #app {
   font-family : Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing : antialiased;
---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg>
+    <use :xlink:href="name" />
+  </svg>
+</template>
+
+<script lang="ts" scoped>
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  return requireContext.keys().forEach(requireContext);
+};
+try {
+  importAll(require.context("../assets/icons/", true, /\.svg$/));
+} catch (err) {
+  console.log(err);
+}
+
+export default {
+  props: ["name"],
+  name: "Icon",
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/css/icon.scss';
+
+</style>
