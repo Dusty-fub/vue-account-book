ac1d1b7631f9e8900fe3a02dacc2aa9d00b2cf79
Dusty <wajtejprotl@outlook.com>
2020-10-02 20:31:16 +0800

    save localstorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,7 +3,7 @@
     <Layout>
       <Tags :dataSource.sync="tags" @update:value="onUpdateTags" />
       <Notes @update:value="onUpdateNotes" />
-      <Types :type.sync="recode.type" />
+      <Types :type.sync="record.type" />
       <NumberPad @update:value="onUpdateAmount" />
     </Layout>
   </div>
@@ -11,18 +11,21 @@
 
 <script lang='ts'>
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 import Layout from "@/components/Layout.vue";
 import Notes from "@/components/Money/Notes.vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 
-type Recode = {
+window.localStorage.setItem("version", "0.0.1");
+
+type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createdAt?: Date;
 };
 
 @Component({
@@ -33,23 +36,37 @@ export default class Money extends Vue {
 
   tags = ["衣", "食", "住"];
 
-  recode: Recode = {
+  record: Record = {
     tags: [],
     notes: "",
     type: "-",
     amount: 0,
   };
+  recordList: Record[] = JSON.parse(
+    window.localStorage.getItem("recordList") || "[]"
+  );
 
   onUpdateTags(value: string[]) {
-    this.recode.tags = value;
+    this.record.tags = value;
   }
   onUpdateNotes(value: string) {
-    this.recode.notes = value;
+    this.record.notes = value;
   }
 
   onUpdateAmount(value: string) {
-    this.recode.amount = Number(value);
-    console.log(this.recode.amount);
+    this.record.amount = Number(value);
+    this.saveRecord();
+  }
+  saveRecord() {
+    const recordCopy: Record = JSON.parse(JSON.stringify(this.record));
+    recordCopy.createdAt = new Date();
+    this.recordList.push(recordCopy);
+  }
+
+  @Watch("recordList")
+  onRecordListChange() {
+    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    console.log(this.recordList);
   }
 }
 </script>
